3045689d47e324eadc274275ef3f85e6d31a8b49
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-07-24 10:15:52 +0900

    2023/07/24(App Settings)

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const preloadAssets = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/logo.png")];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    Promise.all([...fontPromises, ...imagePromises]);
+  };
+  useEffect(() => {
+    const preload = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        preloadAssets();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+    preload();
+  }, []);
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <Text>This is NomadCoffee's Main Page</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
